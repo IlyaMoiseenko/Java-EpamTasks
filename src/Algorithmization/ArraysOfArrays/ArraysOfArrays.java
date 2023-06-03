0a2020ccdaa9041cc582fdd6e2dd6277eb2405af
IlyaMoiseenko <ilya.moiseenko.00@yandex.by>
2023-06-03 20:18:11 +0300

    Algorithmization.ArraysOfArrays.task_1

---
+++ src/Algorithmization/ArraysOfArrays/ArraysOfArrays.java
@@ -0,0 +1,18 @@
+package Algorithmization.ArraysOfArrays;
+
+public class ArraysOfArrays {
+    /*
+    Дана матрица. Вывести на экран все нечетные столбцы,
+    у которых первый элемент больше последнего.
+    */
+    public void task_1(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j += 2) {
+                if (matrix[0][j] > matrix[matrix.length - 1][j]) {
+                    System.out.print(matrix[i][j] + " | ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
