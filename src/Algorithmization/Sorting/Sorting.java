fe46ad20e78e985dca78ca670989d92ac3d23862
Илья Моисеенко <ilyamoiseenko@IlyaMoiseenko.local>
2023-06-25 15:29:27 +0300

    Algorithmization.Sorting.task_2

--- src/Algorithmization/Sorting/Sorting.java
+++ src/Algorithmization/Sorting/Sorting.java
@@ -21,4 +21,21 @@ public class Sorting {
 
         return array1;
     }
+
+    /*
+    Даны две последовательности a1 < a2 < a3 и b1 < b2 < b3.
+    Образовать из них новую последовательность чисел так, чтобы она тоже была неубывающей.
+    Примечание. Дополнительный массив не использовать.
+    */
+    public int[] task_2(int[] array1, int[] array2) {
+        array1 = Arrays.copyOf(array1, array1.length + array2.length);
+
+        for (int i = 0; i < array2.length; i++) {
+            array1[array1.length - array2.length + i] = array2[i];
+        }
+
+        Arrays.sort(array1);
+
+        return array1;
+    }
 }
